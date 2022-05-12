31c8ccf396c4a7e3e6baebec54551f8150c110e8
Ciro Gomes <ciro.ggj@gmail.com>
2022-05-12 20:35:02 -0300

    criando corpo ⚙️

--- index.py
+++ index.py
@@ -11,24 +11,31 @@ largura = 640
 altura = 480
 
 # centrlizado objeto na tela
-x = int(largura / 2)
-y = int(altura / 2)
+x_cobra = int(largura / 2)
+y_cobra = int(altura / 2)
 
-x_verde = randint(40, 600)
-y_verde = randint(50, 430)
+x_fruta = randint(40, 600)
+y_fruta = randint(50, 430)
 
 pontos = 0
+
+lista_cobra = []
 
 fonte = pygame.font.SysFont('arial', 18, True, False)
 
 # Defenindo largura e altura da tela
 tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('Mini-Game')  # Definindo nome do jogo
+pygame.display.set_caption('Snake Game')  # Definindo nome do jogo
 relogio = pygame.time.Clock()  # Frame
 
 
-# loop do jogo
+def aumentando_cobra(lista_cobra):
+    for XeY in lista_cobra:
+        # XeY = []
+        pygame.draw.rect(tela, (0, 255, 0), (XeY[0], XeY[1], 20, 20))
 
+
+# loop do jogo
 while True:
 
     relogio.tick(60)  # Frame
@@ -47,35 +54,40 @@ while True:
 
     # Movimentação por telcas
     if pygame.key.get_pressed()[K_a]:
-        x = x - 20
+        x_cobra = x_cobra - 20
     if pygame.key.get_pressed()[K_LEFT]:
-        x = x - 20
+        x_cobra = x_cobra - 20
     if pygame.key.get_pressed()[K_d]:
-        x = x + 20
+        x_cobra = x_cobra + 20
     if pygame.key.get_pressed()[K_RIGHT]:
-        x = x + 20
+        x_cobra = x_cobra + 20
     if pygame.key.get_pressed()[K_w]:
-        y = y - 20
+        y_cobra = y_cobra - 20
     if pygame.key.get_pressed()[K_UP]:
-        y = y - 20
+        y_cobra = y_cobra - 20
     if pygame.key.get_pressed()[K_s]:
-        y = y + 20
+        y_cobra = y_cobra + 20
     if pygame.key.get_pressed()[K_DOWN]:
-        y = y + 20
-   
-   
+        y_cobra = y_cobra + 20
 
-    ret_vermelho = pygame.draw.rect(tela, (255, 0, 0), (x, y, 40, 50))
-    ret_verde = pygame.draw.rect(
-        tela, (000, 255, 0), (x_verde, y_verde, 40, 50))
+    cobra = pygame.draw.rect(tela, (0, 255, 0), (x_cobra, y_cobra, 20, 20))
+    fruta = pygame.draw.rect(
+        tela, (255, 0, 0), (x_fruta, y_fruta, 20, 20))
 
     # Colisao - mudando posição do ret_verde
-    if ret_vermelho.colliderect(ret_verde):
-        x_verde = randint(40, 600)
-        y_verde = randint(50, 430)
+    if cobra.colliderect(fruta):
+        x_fruta = randint(40, 600)
+        y_fruta = randint(50, 430)
 
         pontos = pontos + 1
-    
+
+    # Criando corpo da cobra
+    lista_cabeca = []
+    lista_cabeca.append(x_cobra)
+    lista_cabeca.append(y_cobra)
+    lista_cobra.append(lista_cabeca)
+    aumentando_cobra(lista_cobra)
+
 
     tela.blit(textoFormatado, (20, 20))
     pygame.display.update()
